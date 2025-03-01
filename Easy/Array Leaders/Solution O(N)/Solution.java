75da75a97add085c461dea8c20d27e044a3f7a01
Aviral Varshney <aviralvarshney07@gmail.com>
2025-03-01 18:01:03 +0530

    Array Leaders using O(N^2) [Gives TLE]

---
+++ Easy/Array Leaders/Solution O(N)/Solution.java
@@ -0,0 +1,54 @@
+/*You are given an array arr of positive integers. Your task is to find all the leaders in the array. An element is considered a leader if it is greater than or equal to all elements to its right. The rightmost element is always a leader.
+
+Examples:
+
+Input: arr = [16, 17, 4, 3, 5, 2]
+Output: [17, 5, 2]
+Explanation: Note that there is nothing greater on the right side of 17, 5 and, 2.
+Input: arr = [10, 4, 2, 4, 1]
+Output: [10, 4, 4, 1]
+Explanation: Note that both of the 4s are in output, as to be a leader an equal element is also allowed on the right. side
+Input: arr = [5, 10, 20, 40]
+Output: [40]
+Explanation: When an array is sorted in increasing order, only the rightmost element is leader.
+Input: arr = [30, 10, 10, 5]
+Output: [30, 10, 10, 5]
+Explanation: When an array is sorted in non-increasing order, all elements are leaders. */
+
+import java.util.*;
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        // code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        int n = arr.length;
+        
+        int maxRight = arr[n - 1]; // Rightmost element is always a leader
+        ans.add(maxRight);
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= maxRight) {
+                maxRight = arr[i];
+                ans.add(maxRight);
+            }
+        }
+
+
+        Collections.reverse(ans);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter the size of the array");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        ArrayList<Integer>result =  leaders(arr);
+        System.out.println("Leaders in the array are:"+result);
+        sc.close();
+    }
+}
+// Time Complexity: O(n)
