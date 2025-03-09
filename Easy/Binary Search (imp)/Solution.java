024f1b4e1a3755c712dd22ef7bca982b0f61b873
Aviral Varshney <aviralvarshney07@gmail.com>
2025-03-09 20:20:24 +0530

    Binary Search and return first occurence

---
+++ Easy/Binary Search (imp)/Solution.java
@@ -0,0 +1,51 @@
+// Given a sorted array arr and an integer k, find the position(0-based indexing) at which k is present in the array using binary search.
+
+// Note: If multiple occurrences are there, please return the smallest index.
+
+// Examples:
+
+// Input: arr[] = [1, 2, 3, 4, 5], k = 4
+// Output: 3
+// Explanation: 4 appears at index 3.
+// Input: arr[] = [11, 22, 33, 44, 55], k = 445
+// Output: -1
+// Explanation: 445 is not present.
+// Input: arr[] = [1, 1, 1, 1, 2], k = 1
+// Output: 0
+// Explanation: 1 appears at index 0.
+import java.util.Scanner;
+class Solution {
+    static int binarysearch(int[] arr, int k) {
+        int low = 0, high = arr.length - 1;
+        int result = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == k) {
+                result = mid;
+                high = mid - 1;
+            } else if (arr[mid] < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array");
+        int n = sc.nextInt();
+        System.out.println("Enter the value of k");
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(binarysearch(arr, k));
+        sc.close();
+    }
+}
