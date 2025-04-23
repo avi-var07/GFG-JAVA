d2514528639a1b9e4dcdd1d194fc1e5c396b20af
Aviral Varshney <aviralvarshney07@gmail.com>
2025-04-23 12:01:45 +0530

    Word with maximum frequency

--- Easy/Word with maximum frequency/Solution.java
+++ Easy/Word with maximum frequency/Solution.java
@@ -1,3 +1,14 @@
+/*You are given a string s that is made up of words separated by spaces. Your task is to find the word with the highest frequency, i.e. it appears the most times in the sentence. If multiple words have maximum frequency, then print the word that occurs first in the sentence.
+
+Examples:
+
+Input: s = "the devil in the sky"
+Output: "the 2"
+Explanation: The frequency of "the" is 2, so we return "the" and its frequency "2" i.e., "the 2" 
+Input: s = "this is not right"
+Output: "this 1"
+Explanation: Every word has the frequency of "1", so we return "this 1" as this occurs first in the sentence.  */
+
 import java.util.*;
 
 class Solution {
