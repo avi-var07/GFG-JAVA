cd8ec9dd03a5a24a4901c8de46e0d8183fcfcb46
Aviral Varshney <aviralvarshney07@gmail.com>
2025-08-15 21:34:28 +0530

    Lower Bound

---
+++ Easy/Implement Lower Bound/Solution.java
@@ -0,0 +1,51 @@
+/*Given a sorted array arr[] and a number target, the task is to find the lower bound of the target in this given array. The lower bound of a number is defined as the smallest index in the sorted array where the element is greater than or equal to the given number.
+
+Note: If all the elements in the given array are smaller than the target, the lower bound will be the length of the array. 
+
+Examples :
+
+Input:  arr[] = [2, 3, 7, 10, 11, 11, 25], target = 9
+Output: 3
+Explanation: 3 is the smallest index in arr[] where element (arr[3] = 10) is greater than or equal to 9.
+Input: arr[] = [2, 3, 7, 10, 11, 11, 25], target = 11
+Output: 4
+Explanation: 4 is the smallest index in arr[] where element (arr[4] = 11) is greater than or equal to 11.
+Input: arr[] = [2, 3, 7, 10, 11, 11, 25], target = 100
+Output: 7
+Explanation: As no element in arr[] is greater than 100, return the length of array.
+Constraints:
+1 ≤ arr.size() ≤ 106
+1 ≤ arr[i] ≤ 106
+1 ≤ target ≤ 106 */
+
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter " + n + " elements:");
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) 
+        arr[i] = sc.nextInt();
+        System.out.println("Enter target: ");
+        int target = sc.nextInt();
+        System.out.println("Lower Bound is: "+lowerBound(arr, target));
+        
+        sc.close();
+    }
+    static int lowerBound(int[] arr, int target) {
+        // code here
+        int low = 0, high=arr.length-1, ans=arr.length;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(arr[mid]>=target){
+                ans=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return ans;
+    }
+}
