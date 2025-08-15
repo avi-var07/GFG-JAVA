664ef59f47d358c7b6f6158ede24557564430681
Aviral Varshney <aviralvarshney07@gmail.com>
2025-08-15 15:23:01 +0530

    Minimum days to make M bouquets

---
+++ Medium/Minimum days to make M bouquets/Solution.java
@@ -0,0 +1,71 @@
+/*You have a row of flowers, where each flower blooms after a specific day. The array arr[] represents the blooming schedule: arr[i] is the day the flower at position i will bloom. To create a bouquet, you need to collect k adjacent bloomed flowers. Each flower can only be used in one bouquet.
+
+Your goal is to find the minimum number of days required to make exactly m bouquets. If it is not possible to make m bouquets with the given arrangement, return -1.
+
+Examples:
+Input: m = 3, k = 2, arr[] = [3, 4, 2, 7, 13, 8, 5]
+Output: 8
+Explanation: We need 3 bouquets and each bouquet should have 2 flowers. After day 8: [x, x, x, x, _, x, x], we can make first bouquet from the first 2 flowers, second bouquet from the next 2 flowers and the third bouquet from the last 2 flowers.
+Input: m = 2, k = 3, arr[] = [5, 5, 5, 5, 10, 5, 5]
+Output: 10
+Explanation: We need 2 bouquets and each bouquet should have 3 flowers, After day 5: [x, x, x, x, _, x, x], we can make one bouquet of the first three flowers that bloomed, but cannot make another bouquet. After day 10: [x, x, x, x, x, x, x], Now we can make two bouquets, taking 3 adjacent flowers in one bouquet.
+Input: m = 3, k = 2, arr[] = [1, 10, 3, 10, 2]
+Output: -1
+Explanation: As 3 bouquets each having 2 flowers are needed, that means we need 6 flowers. But there are only 5 flowers so it is impossible to get the needed bouquets therefore -1 will be returned.
+Constraints:
+1 ≤ k ≤ arr.size() ≤ 105
+1 ≤ m ≤ 105
+1 ≤ arr[i] ≤ 109 */
+
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter " + n + " elements:");
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) 
+        arr[i] = sc.nextInt();
+        
+        System.out.println("Enter no of adjacent flowers to choose: ");
+        int k = sc.nextInt();
+
+        System.out.println("Enter number of bouquets you need: ");
+        int m = sc.nextInt();
+
+        System.out.println("Minimum no of days required: "+minDaysBloom(arr, k, m));
+        
+        sc.close();
+    }
+    public static int minDaysBloom(int[] arr, int k, int m) {
+        // code here
+        if(k*m>arr.length)return -1;
+        int low = arr[0], high = arr[0];
+        for(int ele: arr){
+            if(ele>high)high=ele;
+            if(ele<low)low=ele;
+        }
+        
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(bouquets(arr, mid, k, m)<m)low=mid+1;
+            else high=mid-1;
+        }
+        return low;
+    }
+    static int bouquets(int arr[], int day, int k, int m){
+        int bloomed = 0, noOfBouquets = 0;
+        
+        for(int ele: arr){
+            if(day>=ele)bloomed++;
+            else{
+                noOfBouquets+=bloomed/k;
+                bloomed=0;
+            }
+        }
+        noOfBouquets+=bloomed/k;
+        return noOfBouquets;
+    }
+}
