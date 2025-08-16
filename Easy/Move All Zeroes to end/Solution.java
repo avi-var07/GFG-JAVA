2b4c3cf0649fe6eb016268df021306fc72523375
Aviral Varshney <aviralvarshney07@gmail.com>
2025-08-17 01:16:17 +0530

    Move Zeroes to end

---
+++ Easy/Move All Zeroes to end/Solution.java
@@ -0,0 +1,57 @@
+/*You are given an array arr[] of non-negative integers. You have to move all the zeros in the array to the right end while maintaining the relative order of the non-zero elements. The operation must be performed in place, meaning you should not use extra space for another array.
+
+Examples:
+
+Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+Output: [1, 2, 4, 3, 5, 0, 0, 0]
+Explanation: There are three 0s that are moved to the end.
+Input: arr[] = [10, 20, 30]
+Output: [10, 20, 30]
+Explanation: No change in array as there are no 0s.
+Input: arr[] = [0, 0]
+Output: [0, 0]
+Explanation: No change in array as there are all 0s.
+Constraints:
+1 ≤ arr.size() ≤ 105
+0 ≤ arr[i] ≤ 105 */
+
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter " + n + " elements:");
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) 
+        arr[i] = sc.nextInt();
+        
+        pushZerosToEnd(arr);
+
+        for(int ele: arr)
+        System.out.print(ele+" ");
+        sc.close();
+    }
+
+    static void pushZerosToEnd(int[] arr) {
+        // code here
+        int j=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1)return;
+        
+        for(int i=j+1;i<arr.length;i++){
+            if(arr[i]!=0){
+                int temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+        }
+    }
+}
