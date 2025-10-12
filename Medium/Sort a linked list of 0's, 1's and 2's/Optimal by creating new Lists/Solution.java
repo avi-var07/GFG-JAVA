5b92a82f57d12d9aacdb50e1d2e9cc7f09963c64
Aviral Varshney <aviralvarshney07@gmail.com>
2025-10-12 15:58:07 +0530

    Sort 0's 1's 2's using seggregate

---
+++ Medium/Sort a linked list of 0's, 1's and 2's/Optimal by creating new Lists/Solution.java
@@ -0,0 +1,98 @@
+/*Given the head of a linked list where nodes can contain values 0s, 1s, and 2s only. Your task is to rearrange the list so that all 0s appear at the beginning, followed by all 1s, and all 2s are placed at the end.
+
+Examples:
+
+Input: head = 1 → 2 → 2 → 1 → 2 → 0 → 2 → 2
+   
+Output: 0 → 1 → 1 → 2 → 2 → 2 → 2 → 2
+Explanation: All the 0s are segregated to the left end of the linked list, 2s to the right end of the list, and 1s in between. The final list will be:
+   
+Input: head = 2 → 2 → 0 → 1
+   
+Output: 0 → 1 → 2 → 2
+Explanation: After arranging all the 0s, 1s and 2s in the given format, the output will be:
+   
+Constraints:
+1 ≤ no. of nodes ≤ 106
+0 ≤ node->data ≤ 2 */
+
+import java.util.Scanner;
+
+class Node {
+    Node next;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter length of the LinkedList: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter " + n + " elements of the LinkedList: ");
+        int val = sc.nextInt();
+        Node head = new Node(val);
+        Node current = head;
+        for (int i = 1; i < n; i++) {
+            int ele = sc.nextInt();
+            Node newNode = new Node(ele);
+            newNode.next = null;
+            current.next = newNode;
+            current = newNode;
+        }
+        print(head);
+        System.out.println();
+        head = segregate(head);
+        print(head);
+        sc.close();
+    }
+    static void print(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "  ");
+            temp = temp.next;
+        }
+    }
+    public static Node segregate(Node head) {
+        // code here
+        Node zeroDummy = new Node(-1);
+        Node oneDummy = new Node(-1);
+        Node twoDummy = new Node(-1);
+        
+        Node t0=zeroDummy;
+        Node t1=oneDummy;
+        Node t2=twoDummy;
+        
+        Node temp =head;
+        while(temp!=null){
+            if(temp.data==0){
+                Node newNode = new Node(0);
+                t0.next=newNode;
+                t0=newNode;
+            }
+            else if(temp.data==1){
+                Node newNode = new Node(1);
+                t1.next=newNode;
+                t1=newNode;
+            }
+            else{
+                Node newNode= new Node(2);
+                t2.next=newNode;
+                t2=newNode;
+            }
+            temp=temp.next;
+        }
+        t0.next=(oneDummy.next==null)?twoDummy.next:oneDummy.next;
+        t1.next=(twoDummy.next==null)?null:twoDummy.next;
+        
+        return zeroDummy.next;
+        
+    }
+}
