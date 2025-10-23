41ccc1e5ee1961616f1b3466095738e11b5c2372
Aviral Varshney <aviralvarshney07@gmail.com>
2025-10-23 12:15:49 +0530

    Generate Binary Strings

---
+++ Medium/Generate Binary String/Solution.java
@@ -0,0 +1,69 @@
+/*Given a string containing of 0, 1 and ? - a wildcard character, generate all distinct binary strings that can be formed by replacing each wildcard character by either 0 or 1.
+
+Example 1:
+
+Input: 1??0?101
+Output: 10000101 10001101 10100101 10101101 
+11000101 11001101 11100101 11101101
+Explanation:
+There will be 8 such possible strings that 
+can be formed, they are 10000101, 10001101, 
+10100101, 10101101, 11000101, 11001101, 
+11100101 and 11101101.
+Example 2:
+
+Input: 10?
+Output: 100 101
+Explanation: There are 2 such possible strings
+and they are 100 and 101.
+Your Task:
+You don't need to read or print anything. Your task is to complete the function generate_binary_string() which takes the given string as input parameter and returns a vector of strings containing all the possible strings that can be formed.
+
+Note : Strings should be printed in lexicographically increasing order.
+
+Expected Time complexity: O(2n)
+Expected Space complexity: O(n*2n)
+
+Constraints:
+1 ≤ length of string ≤ 30
+Note: Number of '?' in any string does not exceed 15. */
+
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String: ");
+        String s = sc.nextLine();
+        List<String> ans = generate_binary_string(s);
+        for(var ele: ans)System.out.print(ele+" ");
+
+        sc.close();
+    }
+    public static List<String> generate_binary_string(String s) {
+        // Code here
+        List<String>ans = new ArrayList<>();
+        char arr[] = s.toCharArray();
+        generate(arr, ans, 0);
+        Collections.sort(ans);
+        return ans;
+        
+    }
+    static void generate(char arr[], List<String>ans, int index){
+        if(index==arr.length){
+            ans.add(new String(arr));
+            return;
+        }
+        
+        if(arr[index]=='?'){
+            arr[index]='0';
+            generate(arr, ans, index+1);
+            arr[index]='?';
+            arr[index]='1';
+            generate(arr, ans, index+1);
+            arr[index]='?';
+            
+        }
+        else generate(arr, ans, index+1);
+    }
+}
