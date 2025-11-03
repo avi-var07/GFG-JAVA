924f1544bc55dcb35fc5c880c13d3eae5cf41645
Aviral Varshney <aviralvarshney07@gmail.com>
2025-11-03 12:16:15 +0530

    Postfix to infix

---
+++ Medium/Postfix to Infix/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Postfix: ");
+        String postfix = sc.nextLine();
+        System.out.println("Infix is: "+postfixToInfix(postfix));
+        sc.close();
+    }
+    static String postfixToInfix(String str){
+        Stack<String>st  = new Stack<>();
+        for(char ch:  str.toCharArray()){
+            if((ch>='A'&&ch<='Z')||(ch>='a'&&ch<='z')||(ch>='0'&&ch<='9'))st.push(Character.toString(ch));
+            else{
+                String t1 = st.pop();
+                String t2 = st.pop();
+                String con  = '('+t2+ch+t1+')';
+                st.push(con);
+            }
+        }
+        return st.peek();
+    }
+}
