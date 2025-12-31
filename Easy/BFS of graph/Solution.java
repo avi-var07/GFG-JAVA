c084b2dfeff15a3b88ff3817f63eaf83e1ba2add
Aviral Varshney <aviralvarshney07@gmail.com>
2025-12-31 18:55:40 +0530

    Important changes

--- Easy/BFS of graph/Solution.java
+++ Easy/BFS of graph/Solution.java
@@ -1,3 +1,30 @@
+/*Given a connected undirected graph containing V vertices, represented by a 2-d adjacency list adj[][], where each adj[i] represents the list of vertices connected to vertex i. Perform a Breadth First Search (BFS) traversal starting from vertex 0, visiting vertices from left to right according to the given adjacency list, and return a list containing the BFS traversal of the graph.
+
+Note: Do traverse in the same order as they are in the given adjacency list.
+
+Examples:
+
+Input: adj[][] = [[2, 3, 1], [0], [0, 4], [0], [2]]
+
+Output: [0, 2, 3, 1, 4]
+Explanation: Starting from 0, the BFS traversal will follow these steps: 
+Visit 0 → Output: 0 
+Visit 2 (first neighbor of 0) → Output: 0, 2 
+Visit 3 (next neighbor of 0) → Output: 0, 2, 3 
+Visit 1 (next neighbor of 0) → Output: 0, 2, 3, 1
+Visit 4 (neighbor of 2) → Final Output: 0, 2, 3, 1, 4
+Input: adj[][] = [[1, 2], [0, 2], [0, 1, 3, 4], [2], [2]]
+
+Output: [0, 1, 2, 3, 4]
+Explanation: Starting from 0, the BFS traversal proceeds as follows: 
+Visit 0 → Output: 0 
+Visit 1 (the first neighbor of 0) → Output: 0, 1 
+Visit 2 (the next neighbor of 0) → Output: 0, 1, 2 
+Visit 3 (the first neighbor of 2 that hasn't been visited yet) → Output: 0, 1, 2, 3 
+Visit 4 (the next neighbor of 2) → Final Output: 0, 1, 2, 3, 4
+Constraints:
+1 ≤ V = adj.size() ≤ 104
+0 ≤ adj[i][j] ≤ 104 */
 import java.util.*;
 
 class Solution{
@@ -19,37 +46,26 @@ class Solution{
             adj.get(v).add(u);
 
         }
-    /*Printing Adjacency List
 
-        for(int i=1;i<=n;i++){
-            System.out.print(i+" ->");
-            for(int j=0;j<adj.get(i).size();j++)System.out.print(adj.get(i).get(j)+" ");
+        ArrayList<Integer>BFS = bfs(adj);
 
-            System.out.println();
-        }
-
-    */
-
-        ArrayList<Integer>bfs = BFS(adj);
-
-        for(var ele: bfs)System.out.print(ele+" ");
+        for(var ele: BFS)System.out.print(ele+" ");
         sc.close();
     }
-    static ArrayList<Integer> BFS(ArrayList<ArrayList<Integer>>adj){
-
-        int V = adj.size(); //no of nodes nikalne ka trika
+    static public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
         Queue<Integer>queue = new LinkedList<>();
+        int V = adj.size();
         boolean visited[] = new boolean[V];
-        queue.add(1);
-        visited[1]=true;
-
         ArrayList<Integer>bfs = new ArrayList<>();
-
+        visited[0]=true;
+        queue.add(0);
+        
         while(!queue.isEmpty()){
-            Integer node = queue.poll();    //nikalo queue se
-            bfs.add(node);  
-
-            for(var ele: adj.get(node)){    //usk saare neighbours fetch kro adj list se
+            Integer node = queue.poll(); 
+            bfs.add(node);
+            
+            for(var ele: adj.get(node)){
                 if(!visited[ele]){
                     visited[ele]=true;
                     queue.add(ele);
