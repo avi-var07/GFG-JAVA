723bef69efd09dfb47897e477f7b43c2ab034448
Aviral Varshney <aviralvarshney07@gmail.com>
2026-01-25 11:01:39 +0530

    Prime Number

---
+++ Easy/Prime Number/Solution.java
@@ -0,0 +1,42 @@
+/*Given a number n, determine whether it is a prime number or not.
+Note: A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
+
+Examples :
+
+Input: n = 7
+Output: true
+Explanation: 7 has exactly two divisors: 1 and 7, making it a prime number.
+Input: n = 25
+Output: false
+Explanation: 25 has more than two divisors: 1, 5, and 25, so it is not a prime number.
+Input: n = 1
+Output: false
+Explanation: 1 has only one divisor (1 itself), which is not sufficient for it to be considered prime.
+Constraints:
+1 ≤ n ≤ 109 */
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+        System.out.println(isPrime(n));
+
+        sc.close();
+    }
+    static boolean isPrime(int n) {
+        // code here
+        boolean isPrime[] = new boolean[n+1];
+        Arrays.fill(isPrime,true);
+        isPrime[0]=false;
+        isPrime[1]=false;
+        for(int i=2;i*i<=n;i++){
+            if(isPrime[i]){
+                for(int j=i*i;j<=n;j+=i)isPrime[j]=false;
+            }
+        }
+        
+        return isPrime[n];
+    }
+}
